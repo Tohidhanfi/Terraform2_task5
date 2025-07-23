fd6e841625c62a40b39f0e3bbeb27ba8e24e9b2c
Tohid Hanfi <73274598+Tohidhanfi@users.noreply.github.com>
2025-07-23 12:53:49 +0530

    Update variables.tf

--- terraform2_task5/variables.tf
+++ terraform2_task5/variables.tf
@@ -24,9 +24,3 @@ variable "image_tag" {
   description = "Docker image tag"
   type        = string
 }
-
-variable "dockerhub_username" {
-  description = "DockerHub username for pulling images"
-  type        = string
-  default     = tohidazure
-}
