3d70b5217802762befd1da2d078250f5b2b646c9
tohidhanfi <tohidhanfi@gmail.com>
2025-07-31 10:53:48 +0530

    Changed file to back to production mode

---
+++ strapi-app/src/admin/vite.config.ts
@@ -0,0 +1,16 @@
+import { mergeConfig, type UserConfig } from 'vite';
+
+export default (config: UserConfig) => {
+  // Important: always return the modified config
+  return mergeConfig(config, {
+    resolve: {
+      alias: {
+        '@': '/src',
+      },
+    },
+    server: {
+      allowedHosts: true, // Allow all hosts
+      host: '0.0.0.0', // Listen on all interfaces
+    },
+  });
+}; 
