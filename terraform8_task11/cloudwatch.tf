7dfb813d2e5d09c820273738b1459abe7e594de7
tohidhanfi <tohidhanfi@gmail.com>
2025-08-01 10:47:11 +0530

    ADDED files for task11 and also updated readme.md file

--- terraform8_task11/cloudwatch.tf
+++ terraform8_task11/cloudwatch.tf
@@ -1,6 +1,6 @@
 # CloudWatch Dashboard for Strapi ECS Monitoring
 resource "aws_cloudwatch_dashboard" "strapi_dashboard" {
-  dashboard_name = "Strapi-task9-ECS-Dashboard"
+  dashboard_name = "Strapi-task11-ECS-Dashboard"
 
   dashboard_body = jsonencode({
     widgets = [
@@ -12,7 +12,7 @@ resource "aws_cloudwatch_dashboard" "strapi_dashboard" {
         height = 6
         properties = {
           metrics = [
-            ["AWS/ECS", "CPUUtilization", "ServiceName", aws_ecs_service.tohid_service.name, "ClusterName", aws_ecs_cluster.tohid_cluster.name],
+            ["AWS/ECS", "CPUUtilization", "ServiceName", aws_ecs_service.main.name, "ClusterName", aws_ecs_cluster.main.name],
             [".", "MemoryUtilization", ".", ".", ".", "."]
           ]
           period = 300
@@ -29,7 +29,7 @@ resource "aws_cloudwatch_dashboard" "strapi_dashboard" {
         height = 6
         properties = {
           metrics = [
-            ["AWS/ECS", "RunningTaskCount", "ServiceName", aws_ecs_service.tohid_service.name, "ClusterName", aws_ecs_cluster.tohid_cluster.name],
+            ["AWS/ECS", "RunningTaskCount", "ServiceName", aws_ecs_service.main.name, "ClusterName", aws_ecs_cluster.main.name],
             [".", "PendingTaskCount", ".", ".", ".", "."]
           ]
           period = 300
@@ -46,7 +46,7 @@ resource "aws_cloudwatch_dashboard" "strapi_dashboard" {
         height = 6
         properties = {
           metrics = [
-            ["AWS/ECS", "NetworkRxBytes", "ServiceName", aws_ecs_service.tohid_service.name, "ClusterName", aws_ecs_cluster.tohid_cluster.name],
+            ["AWS/ECS", "NetworkRxBytes", "ServiceName", aws_ecs_service.main.name, "ClusterName", aws_ecs_cluster.main.name],
             [".", "NetworkTxBytes", ".", ".", ".", "."]
           ]
           period = 300
@@ -63,7 +63,7 @@ resource "aws_cloudwatch_dashboard" "strapi_dashboard" {
         height = 6
         properties = {
           metrics = [
-            ["AWS/ApplicationELB", "TargetResponseTime", "LoadBalancer", aws_lb.tohid_alb.arn_suffix, "TargetGroup", aws_lb_target_group.tohid_tg.arn_suffix],
+            ["AWS/ApplicationELB", "TargetResponseTime", "LoadBalancer", aws_lb.main.arn_suffix, "TargetGroup", aws_lb_target_group.blue.arn_suffix],
             [".", "RequestCount", ".", ".", ".", "."]
           ]
           period = 300
@@ -80,7 +80,7 @@ resource "aws_cloudwatch_dashboard" "strapi_dashboard" {
         height = 6
         properties = {
           metrics = [
-            ["AWS/ApplicationELB", "HTTPCode_Target_2XX_Count", "LoadBalancer", aws_lb.tohid_alb.arn_suffix, "TargetGroup", aws_lb_target_group.tohid_tg.arn_suffix],
+            ["AWS/ApplicationELB", "HTTPCode_Target_2XX_Count", "LoadBalancer", aws_lb.main.arn_suffix, "TargetGroup", aws_lb_target_group.blue.arn_suffix],
             [".", "HTTPCode_Target_4XX_Count", ".", ".", ".", "."],
             [".", "HTTPCode_Target_5XX_Count", ".", ".", ".", "."]
           ]
@@ -98,7 +98,7 @@ resource "aws_cloudwatch_dashboard" "strapi_dashboard" {
         height = 6
         properties = {
           metrics = [
-            ["AWS/RDS", "CPUUtilization", "DBInstanceIdentifier", aws_db_instance.tohid_rds.id],
+            ["AWS/RDS", "CPUUtilization", "DBInstanceIdentifier", aws_db_instance.main.id],
             [".", "DatabaseConnections", ".", "."],
             [".", "FreeableMemory", ".", "."]
           ]
@@ -116,7 +116,7 @@ resource "aws_cloudwatch_dashboard" "strapi_dashboard" {
 
 # High CPU Utilization Alarm
 resource "aws_cloudwatch_metric_alarm" "ecs_cpu_high" {
-  alarm_name          = "strapi-ecs-cpu-high"
+  alarm_name          = "strapi-task11-ecs-cpu-high"
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = "2"
   metric_name         = "CPUUtilization"
@@ -128,14 +128,14 @@ resource "aws_cloudwatch_metric_alarm" "ecs_cpu_high" {
   alarm_actions       = []
 
   dimensions = {
-    ServiceName = aws_ecs_service.tohid_service.name
-    ClusterName = aws_ecs_cluster.tohid_cluster.name
+    ServiceName = aws_ecs_service.main.name
+    ClusterName = aws_ecs_cluster.main.name
   }
 }
 
 # High Memory Utilization Alarm
 resource "aws_cloudwatch_metric_alarm" "ecs_memory_high" {
-  alarm_name          = "strapi-ecs-memory-high"
+  alarm_name          = "strapi-task11-ecs-memory-high"
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = "2"
   metric_name         = "MemoryUtilization"
@@ -147,14 +147,14 @@ resource "aws_cloudwatch_metric_alarm" "ecs_memory_high" {
   alarm_actions       = []
 
   dimensions = {
-    ServiceName = aws_ecs_service.tohid_service.name
-    ClusterName = aws_ecs_cluster.tohid_cluster.name
+    ServiceName = aws_ecs_service.main.name
+    ClusterName = aws_ecs_cluster.main.name
   }
 }
 
 # Task Count Alarm (Service Down)
 resource "aws_cloudwatch_metric_alarm" "ecs_task_count" {
-  alarm_name          = "strapi-ecs-task-count"
+  alarm_name          = "strapi-task11-ecs-task-count"
   comparison_operator = "LessThanThreshold"
   evaluation_periods  = "2"
   metric_name         = "RunningTaskCount"
@@ -166,14 +166,14 @@ resource "aws_cloudwatch_metric_alarm" "ecs_task_count" {
   alarm_actions       = []
 
   dimensions = {
-    ServiceName = aws_ecs_service.tohid_service.name
-    ClusterName = aws_ecs_cluster.tohid_cluster.name
+    ServiceName = aws_ecs_service.main.name
+    ClusterName = aws_ecs_cluster.main.name
   }
 }
 
 # ALB Response Time Alarm
 resource "aws_cloudwatch_metric_alarm" "alb_response_time" {
-  alarm_name          = "strapi-alb-response-time"
+  alarm_name          = "strapi-task11-alb-response-time"
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = "2"
   metric_name         = "TargetResponseTime"
@@ -185,14 +185,14 @@ resource "aws_cloudwatch_metric_alarm" "alb_response_time" {
   alarm_actions       = []
 
   dimensions = {
-    LoadBalancer = aws_lb.tohid_alb.arn_suffix
-    TargetGroup  = aws_lb_target_group.tohid_tg.arn_suffix
+    LoadBalancer = aws_lb.main.arn_suffix
+    TargetGroup  = aws_lb_target_group.blue.arn_suffix
   }
 }
 
 # ALB 5XX Error Rate Alarm
 resource "aws_cloudwatch_metric_alarm" "alb_5xx_errors" {
-  alarm_name          = "strapi-alb-5xx-errors"
+  alarm_name          = "strapi-task11-alb-5xx-errors"
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = "2"
   metric_name         = "HTTPCode_Target_5XX_Count"
@@ -204,14 +204,14 @@ resource "aws_cloudwatch_metric_alarm" "alb_5xx_errors" {
   alarm_actions       = []
 
   dimensions = {
-    LoadBalancer = aws_lb.tohid_alb.arn_suffix
-    TargetGroup  = aws_lb_target_group.tohid_tg.arn_suffix
+    LoadBalancer = aws_lb.main.arn_suffix
+    TargetGroup  = aws_lb_target_group.blue.arn_suffix
   }
 }
 
 # RDS CPU Utilization Alarm
 resource "aws_cloudwatch_metric_alarm" "rds_cpu_high" {
-  alarm_name          = "strapi-rds-cpu-high"
+  alarm_name          = "strapi-task11-rds-cpu-high"
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = "2"
   metric_name         = "CPUUtilization"
@@ -223,13 +223,13 @@ resource "aws_cloudwatch_metric_alarm" "rds_cpu_high" {
   alarm_actions       = []
 
   dimensions = {
-    DBInstanceIdentifier = aws_db_instance.tohid_rds.id
+    DBInstanceIdentifier = aws_db_instance.main.id
   }
 }
 
 # RDS Connection Count Alarm
 resource "aws_cloudwatch_metric_alarm" "rds_connections" {
-  alarm_name          = "strapi-rds-connections"
+  alarm_name          = "strapi-task11-rds-connections"
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = "2"
   metric_name         = "DatabaseConnections"
@@ -241,13 +241,13 @@ resource "aws_cloudwatch_metric_alarm" "rds_connections" {
   alarm_actions       = []
 
   dimensions = {
-    DBInstanceIdentifier = aws_db_instance.tohid_rds.id
+    DBInstanceIdentifier = aws_db_instance.main.id
   }
 }
 
 # Custom Metric for Application Health Check
 resource "aws_cloudwatch_metric_alarm" "application_health" {
-  alarm_name          = "strapi-application-health"
+  alarm_name          = "strapi-task11-application-health"
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = "3"
   metric_name         = "HealthyHostCount"
@@ -259,7 +259,7 @@ resource "aws_cloudwatch_metric_alarm" "application_health" {
   alarm_actions       = []
 
   dimensions = {
-    LoadBalancer = aws_lb.tohid_alb.arn_suffix
-    TargetGroup  = aws_lb_target_group.tohid_tg.arn_suffix
+    LoadBalancer = aws_lb.main.arn_suffix
+    TargetGroup  = aws_lb_target_group.blue.arn_suffix
   }
 } 
