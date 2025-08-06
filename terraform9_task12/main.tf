f8ad929bc88a465ec64dd64f9ac42552799b2514
tohidhanfi20 <tohidhanfi20@gmail.com>
2025-08-06 15:25:10 +0530

    updated main.tf

--- terraform9_task12/main.tf
+++ terraform9_task12/main.tf
@@ -454,3 +454,25 @@ resource "aws_iam_role_policy" "codedeploy_ecs_policy" {
     ]
   })
 }
+
+resource "aws_iam_role" "codedeploy_role" {
+  name = "codedeploy_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "codedeploy.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "codedeploy_role_policy" {
+  role       = aws_iam_role.codedeploy_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole"
+}
